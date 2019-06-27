52fe508cecddda6c3a933434e6b2ee8f41022a6f
Samuel Mataraso <samuelmataraso@gmail.com>
2019-06-27 13:22:06 -0300

    feat/ Gifs Grid

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'ui/home_page.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: HomePage(),
     theme: ThemeData(hintColor: Colors.white),
   ));
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -58,9 +58,52 @@ class _HomePageState extends State<HomePage> {
               style: TextStyle(color: Colors.white, fontSize: 18.0),
               textAlign: TextAlign.center,
             ),
-          )
+          ),
+          Expanded(
+            child: FutureBuilder(
+              future: _getGifs(),
+              builder: (context, snapshop) {
+                switch (snapshop.connectionState) {
+                  case ConnectionState.waiting:
+                  case ConnectionState.none:
+                    return Container(
+                      width: 200.0,
+                      height: 200.0,
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                        strokeWidth: 5.0,
+                      ),
+                    );
+                  default:
+                    if (snapshop.hasError)
+                      return Container();
+                    else
+                      return _createGifTable(context, snapshop);
+                }
+              },
+            ),
+          ),
         ],
       ),
+    );
+  }
+
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
+    return GridView.builder(
+      padding: EdgeInsets.all(10.0),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, crossAxisSpacing: 10.0, mainAxisSpacing: 10.0),
+      itemCount: snapshot.data["data"].length,
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          child: Image.network(
+            snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+            height: 300.0,
+            fit: BoxFit.cover,
+          ),
+        );
+      },
     );
   }
 }
